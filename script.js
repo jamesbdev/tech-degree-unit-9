67875baa9d96396a7ff2f78f1168c7422969615d
James Bremner <j.bremner@ymail.com>
2024-02-03 15:58:54 +0000

    add descriptions

--- script.js
+++ script.js
@@ -7,7 +7,43 @@ const modalData = [{
     img: 'assets/images/project-8-desktop.png',
     link_text: 'View project',
     link_url: 'https://jamesbdev.github.io/tech-degree-unit-8/'
-}]
+}, {
+    title: 'Web app dashboard',
+    description: 'In this project I used HTML, CSS, JavaScript to create a dashboard for a fictional company application. I used the graph.js library to create graph models using data. There is also a search auto-complete for the employee messaging feature, a form validation using JavaScript. I also added a way to save the user settings using local storage.',
+    skills: 'HTML, CSS, JavaScript, graph.js',
+    img: 'assets/images/unit-7-project.png',
+    link_text: 'View project',
+    link_url: 'https://jamesbdev.github.io/tech-degree-unit-7/'
+} , {
+    title: 'Game Show App',
+    description: 'I used JavaScript to create a game where the user guesses letters. The game ends with the user guessing the whole phrase or if the user misses 5 times. I used JavaScript to store data for 6 phrases and display them randomly. The code checks if the letter selected by the user matches letters in the phrase. If it does the letters are displayed on the screen.',
+    skills: 'HTML, CSS, JavaScript',
+    img: 'assets/images/unit-6-project.png',
+    link_text: 'View project',
+    link_url: 'https://jamesbdev.github.io/tech-degree-unit-6/'
+}, {
+    title: 'Interactive Photo Gallery',
+    description: ' A photo gallery built using CSS grids to display the images. I used a JavaScript plug in to display a modal when the user clicks on the images. The modal contains the image in full size with a description. I also implemented a searching functionality. It uses JavaScript to match the value of the input with the images descriptions.',
+    skills: 'HTML, CSS, JavaScript',
+    img: 'assets/images/project 5.png',
+    link_text: 'View project',
+    link_url: 'https://jamesbdev.github.io/tech-degree-unit-5/'
+}, {
+    title: 'Web style guide',
+    description: 'Using SCSS to create styling for a web style guide. I used SCSS to create a modular file system. I also used it to store color and breakpoint variables.',
+    skills: 'HTML, SCSS',
+    img: 'assets/images/unit-4-project.png',
+    link_text: 'View project',
+    link_url: 'https://jamesbdev.github.io/tech-degree-unit-4/'
+}, {
+    title: 'Online registration form',
+    description: 'I created and styled a form in HTML and CSS. It works on mobile, tablet and desktop.',
+    skills: 'HTML, CSS',
+    img: 'assets/images/html_form.png',
+    link_text: 'View project',
+    link_url: 'https://jamesbdev.github.io/tech-degree-unit-3/'
+}
+]
 
 //Displays a modal with the project information
 const showModal = (data) => {
